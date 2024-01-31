1a9dee709a367cbdf4bbb42cbc3c5c1afa009c46
ADMIN <admin@ADMINs-MacBook-Pro.local>
2024-01-31 15:43:15 +0800

    login + register

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
 export default function Page() {
-    return <p>Login Page</p>;
+    return (
+        <div className='relative w-screen h-screen'>
+            <p>Login Page</p>
+
+            <Link 
+                href="/register"
+                className="absolute bottom-4 h-8 w-72 left-1/2 -ml-36 border border-slate-300 hover:border-indigo-300 bg-white rounded-lg flex items-center justify-center text-black">
+                <span>Go Register</span>
+            </Link>
+        </div>
+    );
 }
